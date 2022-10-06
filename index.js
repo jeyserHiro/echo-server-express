6f45a039930da1c160dbf495bc986c22da92d804
Hiro <109828238+jeyserHiro@users.noreply.github.com>
2022-10-06 09:19:09 +0000

    new features

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require('express');
-  
+
 const app = express();
 
-// error handler
-app.get("/", (req, res) => {
-  res.json({ "message": "welcome" })
-})
-app.post("/", (req, res) => {
-  res.json({ "message": "welcome" })
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/ping", (req, res) => {
+app.all("/", (req, res) => {
   res.json({ "message": "welcome" })
 })
 
+app.all("/ping", (req, res) => {
+  res.json({ "message": "thanks for pinging" })
+})
 
+app.all("/health", (req, res) => {
+  res.json({ "message": "healthy" })
+})
+
+app.all("/body", (req, res) => {
+  let data = req.body ?? { "error": "no body found" }
+  res.json(data)
+})
+
+app.get("/date", (req, res) => {
+  res.json({date:new Date() })
+})
 
 app.listen(80, () => console.log("server has started on " + 80))
 module.exports = app;
